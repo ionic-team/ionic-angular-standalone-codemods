252cfc5c4ce61a7d44037e37a5a02b6bad602fb1
Sean Perkins <sean@ionic.io>
2023-09-17 22:48:59 -0400

    chore: add standalone app example

---
+++ apps/angular/ionic-angular-standalone/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-angular-standalone',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
