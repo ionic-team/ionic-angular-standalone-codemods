44b7dcb876a55aa0741a546a97e0322f477a9585
Sean Perkins <sean@ionic.io>
2023-09-18 12:55:36 -0400

    chore: clean-up

--- packages/cli/src/angular/migrations/standalone/0003-migrate-bootstrap-application.ts
+++ packages/cli/src/angular/migrations/standalone/0003-migrate-bootstrap-application.ts
@@ -1,5 +1,5 @@
-import type { ObjectLiteralExpression, Project, PropertyAssignment, SourceFile } from "ts-morph";
-import { SyntaxKind, ts } from "ts-morph";
+import type { ObjectLiteralExpression, Project, SourceFile } from "ts-morph";
+import { SyntaxKind } from "ts-morph";
 import type { CliOptions } from "../../../types/cli-options";
 import { saveFileChanges } from "../../utils/log-utils";
 
@@ -68,7 +68,9 @@ export const migrateBootstrapApplication = async (project: Project, cliOptions: 
       return;
     }
 
-    const importProvidersFromFunctionCallIonicModuleForRootCallExpression = importProvidersFromFunctionCallArguments[0];
+    const importProvidersFromFunctionCallIonicModuleForRootCallExpression = importProvidersFromFunctionCallArguments.find(argument => {
+      return argument.getText().includes('IonicModule.forRoot');
+    });
 
     if (importProvidersFromFunctionCallIonicModuleForRootCallExpression === undefined) {
       return;
@@ -80,18 +82,24 @@ export const migrateBootstrapApplication = async (project: Project, cliOptions: 
 
       providersArray.addElement(`provideIonicAngular(${ionicConfigObjectLiteralExpression.getText()})`);
 
-      providersArray.removeElement(importProvidersFromFunctionCall);
+      // Remove the IonicModule.forRoot from the importProvidersFrom function call.
+      importProvidersFromFunctionCall.removeArgument(importProvidersFromFunctionCallIonicModuleForRootCallExpression);
+
+      if (importProvidersFromFunctionCall.getArguments().length === 0) {
+        // If there are no remaining arguments, remove the importProvidersFrom function call.
+        providersArray.removeElement(importProvidersFromFunctionCall);
+      }
 
       providersArray.formatText();
 
-      migrateImports(sourceFile);
+      migrateIonicAngularImportDeclarations(sourceFile);
 
       await saveFileChanges(sourceFile, cliOptions);
     }
   }
 }
 
-function migrateImports(sourceFile: SourceFile) {
+function migrateIonicAngularImportDeclarations(sourceFile: SourceFile) {
   const importDeclaration = sourceFile.getImportDeclaration('@ionic/angular');
 
   if (!importDeclaration) {
