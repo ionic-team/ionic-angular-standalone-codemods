0034989456ad06231834326273fe114d7016ca37
Sean Perkins <sean@ionic.io>
2023-09-22 00:59:57 -0400

    test: migrate import statements

---
+++ packages/cli/src/angular/migrations/standalone/0004-migrate-import-statements.test.ts
@@ -0,0 +1,37 @@
+import { describe, expect, it } from 'vitest';
+import { Project } from 'ts-morph';
+import { dedent } from 'ts-dedent';
+
+import { migrateImportStatements } from './0004-migrate-import-statements';
+
+describe('migrateImportStatements', () => {
+
+  it('should migrate import statements', async () => {
+    const project = new Project({ useInMemoryFileSystem: true });
+
+    const service = dedent(`
+      import { Injectable } from '@angular/core';
+      import { ModalController } from '@ionic/angular';
+
+      @Injectable()
+      export class MyService {
+        constructor(private modalController: ModalController) { }
+      }
+    `);
+
+    const serviceSourceFile = project.createSourceFile('my.service.ts', service);
+
+    await migrateImportStatements(project, { dryRun: false });
+
+    expect(dedent(serviceSourceFile.getText())).toBe(dedent(`
+      import { Injectable } from '@angular/core';
+      import { ModalController } from '@ionic/angular/standalone';
+
+      @Injectable()
+      export class MyService {
+          constructor(private modalController: ModalController) { }
+      }
+    `));
+  });
+
+});
