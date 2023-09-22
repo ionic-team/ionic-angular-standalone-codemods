82953681b968c27cde2e7c855d4e3a661b8cee0d
Sean Perkins <sean@ionic.io>
2023-09-22 14:02:51 -0400

    test: typescript utils

--- packages/cli/src/angular/utils/typescript-utils.test.ts
+++ packages/cli/src/angular/utils/typescript-utils.test.ts
@@ -40,10 +40,7 @@ describe("getOrCreateConstructor", () => {
 
 describe("addImportToClass", () => {
   it("should add an import to a class", () => {
-    const sourceFileContent = `
-      export class Foo {
-      }
-    `;
+    const sourceFileContent = `export class Foo { }`;
 
     const project = new Project({ useInMemoryFileSystem: true });
     const sourceFile = project.createSourceFile("foo.ts", sourceFileContent);
@@ -54,9 +51,60 @@ describe("addImportToClass", () => {
       dedent(`
     import { Component } from "@angular/core";
 
-    export class Foo {
-    }
+    export class Foo { }
     `),
     );
+  });
+
+  it("should add an import to an existing import declaration", () => {
+    const sourceFileContent = `
+    import { Injectable } from "@angular/core";
+    export class Foo { }
+    `;
+
+    const project = new Project({ useInMemoryFileSystem: true });
+    const sourceFile = project.createSourceFile("foo.ts", sourceFileContent);
+
+    addImportToClass(sourceFile, "Component", "@angular/core");
+
+    expect(dedent(sourceFile.getText())).toBe(
+      dedent(`
+    import { Injectable, Component } from "@angular/core";
+    export class Foo { }
+    `),
+    );
+  });
+
+  it("should add multiple imports", () => {
+    const sourceFileContent = `export class Foo { }`;
+
+    const project = new Project({ useInMemoryFileSystem: true });
+    const sourceFile = project.createSourceFile("foo.ts", sourceFileContent);
+
+    addImportToClass(sourceFile, ["Component", "Injectable"], "@angular/core");
+
+    expect(dedent(sourceFile.getText())).toBe(
+      dedent(`
+    import { Component, Injectable } from "@angular/core";
+
+    export class Foo { }
+    `),
+    );
+  });
+
+  it('should do nothing if the source file does not have a class', () => {
+    const sourceFileContent = `
+        export function foo() { }
+      `;
+
+    const project = new Project({ useInMemoryFileSystem: true });
+    const sourceFile = project.createSourceFile("foo.ts", sourceFileContent);
+
+    addImportToClass(sourceFile, "Component", "@angular/core");
+
+    expect(dedent(sourceFile.getText())).toBe(
+      dedent(`
+      export function foo() { }
+      `));
   });
 });
