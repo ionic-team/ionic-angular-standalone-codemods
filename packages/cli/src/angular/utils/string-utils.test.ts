b9d0da98bc7e7e9c66a9d7bc439ebc03c18cfb69
Sean Perkins <sean@ionic.io>
2023-09-21 22:43:41 -0400

    test: string utils

---
+++ packages/cli/src/angular/utils/string-utils.test.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect } from 'vitest';
+
+import { kebabCaseToPascalCase, kebabCaseToCamelCase } from './string-utils';
+
+describe('kebabCaseToPascalCase', () => {
+
+  it('should convert kebab case to pascal case', () => {
+    expect(kebabCaseToPascalCase('foo-bar-baz')).toBe('FooBarBaz');
+  });
+
+});
+
+describe('kebabCaseToCamelCase', () => {
+
+  it('should convert kebab case to camel case', () => {
+    expect(kebabCaseToCamelCase('foo-bar-baz')).toBe('fooBarBaz');
+  });
+
+});
