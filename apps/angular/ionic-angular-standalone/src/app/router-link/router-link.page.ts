3d27184f342a0e3d74f21222aa62ce82f0890c96
Sean Perkins <sean@ionic.io>
2023-10-10 19:36:15 -0400

    feat: migrate routerLink and routerLinkHref directive usages

---
+++ apps/angular/ionic-angular-standalone/src/app/router-link/router-link.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-router-link',
+  templateUrl: 'router-link.page.html',
+  styleUrls: ['router-link.page.scss'],
+  standalone: true,
+  imports: [IonicModule],
+})
+export class RouterLinkPage {
+  constructor() { }
+}
