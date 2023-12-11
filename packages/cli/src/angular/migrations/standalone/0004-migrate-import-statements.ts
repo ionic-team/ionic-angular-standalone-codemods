8727042c401c7e510036ec7d5f4b1bd5d3458fd9
Luke Rogers <8382642+luke-rogers@users.noreply.github.com>
2023-12-11 09:11:39 +0000

    fix: allow only ts files

--- packages/cli/src/angular/migrations/standalone/0004-migrate-import-statements.ts
+++ packages/cli/src/angular/migrations/standalone/0004-migrate-import-statements.ts
@@ -8,42 +8,42 @@ export const migrateImportStatements = async (
   project: Project,
   cliOptions: CliOptions,
 ) => {
-  // Get all typescript source files in the project and update any @ionic/angular imports to @ionic/angular/standalone
-  for (const sourceFile of project.getSourceFiles()) {
-    if (sourceFile.getFilePath().endsWith(".html")) {
-      continue;
-    }
+    // Get all typescript source files in the project and update any @ionic/angular imports to @ionic/angular/standalone
+    for (const sourceFile of project.getSourceFiles()) {
+        if (!sourceFile.getFilePath().endsWith('.ts')) {
+            continue;
+        }
 
-    let hasChanges = false;
+        let hasChanges = false;
 
-    const importDeclarations = sourceFile.getImportDeclarations();
-    importDeclarations.forEach((importDeclaration) => {
-      const moduleSpecifier = importDeclaration.getModuleSpecifierValue();
-      if (moduleSpecifier === "@ionic/angular") {
-        importDeclaration.setModuleSpecifier("@ionic/angular/standalone");
+        const importDeclarations = sourceFile.getImportDeclarations();
+        importDeclarations.forEach((importDeclaration) => {
+            const moduleSpecifier = importDeclaration.getModuleSpecifierValue();
+            if (moduleSpecifier === "@ionic/angular") {
+                importDeclaration.setModuleSpecifier("@ionic/angular/standalone");
 
-        const namedImports = importDeclaration.getNamedImports();
-        const importSpecifier = namedImports.find(
+                const namedImports = importDeclaration.getNamedImports();
+                const importSpecifier = namedImports.find(
           (n) => n.getName() === "IonicModule",
         );
 
-        if (importSpecifier) {
-          if (namedImports.length > 1) {
-            // Remove the IonicModule import specifier.
-            importSpecifier.remove();
-          } else {
-            // If this is the only import specifier, remove the entire import declaration.
-            importDeclaration.remove();
-          }
-          removeImportFromNgModuleDecorator(sourceFile, "IonicModule");
+                if (importSpecifier) {
+                    if (namedImports.length > 1) {
+                        // Remove the IonicModule import specifier.
+                        importSpecifier.remove();
+                    } else {
+                        // If this is the only import specifier, remove the entire import declaration.
+                        importDeclaration.remove();
+                    }
+                    removeImportFromNgModuleDecorator(sourceFile, "IonicModule");
+                }
+
+                hasChanges = true;
+            }
+        });
+
+        if (hasChanges) {
+            await saveFileChanges(sourceFile, cliOptions);
         }
-
-        hasChanges = true;
-      }
-    });
-
-    if (hasChanges) {
-      await saveFileChanges(sourceFile, cliOptions);
     }
-  }
 };
